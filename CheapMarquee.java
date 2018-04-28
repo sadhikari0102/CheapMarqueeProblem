65e166d2af00f0c3498e06f416c43c64f8e1e24c
Sanjay Adhikari <sadhikari0102@ufl.edu>
2018-04-28 02:08:19 -0400

    Update CheapMarquee.java

--- CheapMarquee.java
+++ CheapMarquee.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,13 +9,11 @@ import java.util.Scanner;
 public class CheapMarquee {
 
     int cheapMarquee(String message, int width) {
-        int currentCount=0;
-        int max=0;
-        int strindex=1, arrindex=1;
+        int currentCount=0,max=0,strindex=1,arrindex=1;
         List<Integer> window = new ArrayList<>();
         int[] arr=pixel(message.charAt(0));
         for(int i=0;i<width && strindex<message.length();i++) {
-            if(arrindex==arr.length) {
+            while(arrindex==arr.length) {
                 arr=pixel(message.charAt(strindex));
                 arrindex=0;
                 strindex++;
@@ -25,7 +24,7 @@ public class CheapMarquee {
         }
         max=currentCount;
         while(strindex<message.length()) {
-            if(arrindex==arr.length) {
+            while(arrindex==arr.length) {
                 arr=pixel(message.charAt(strindex));
                 arrindex=0;
                 strindex++;
@@ -36,25 +35,8 @@ public class CheapMarquee {
             max=(max>=currentCount)?max:currentCount;
             arrindex++;
         }
-
         return max;
     }
-/*
-    void printArray(int[] arr) {
-        for(int i=0;i<arr.length;i++)
-            System.out.print(arr[i]);
-        System.out.println();
-    }
-
-    void printWindow(List<Integer> list) {
-        int i=0;
-        while(i<list.size()) {
-            System.out.print(list.get(i));
-            i++;
-        }
-        System.out.println();
-    }
-*/
     int[] pixel(char c) {
         int ch=c;
         if(ch >=97 && ch <=122) {
@@ -63,12 +45,12 @@ public class CheapMarquee {
         }
         switch(c) {
             case 'A': return new int[]{0,4,2,2,4};
-            case 'B':return new int[]{0,5,3,3,2};
+            case 'B': return new int[]{0,5,3,3,2};
             case 'C': return new int[]{0,3,2,2,2};
             case 'D': return new int[]{0,5,2,2,3};
             case 'E': return new int[]{0,5,3,3,2};
             case 'F': return new int[]{0,5,2,2,1};
-            case 'G': return new int[]{0,3,2,2,3};
+            case 'G': return new int[]{0,3,2,3,3};
             case 'H': return new int[]{0,5,1,1,5};
             case 'I': return new int[]{0,2,5,2,2};
             case 'J': return new int[]{0,2,2,4,1};
@@ -103,10 +85,25 @@ public class CheapMarquee {
             case '!': return new int[]{0,4};
             case '?': return new int[]{0,1,3,2,1};
             case ' ': return new int[]{0};
-            default: return new int[]{-1};
+            default: return new int[]{};
         }
     }
+/*
+    void printArray(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i]);
+        System.out.println();
+    }
 
+    void printWindow(List<Integer> list) {
+        int i=0;
+        while(i<list.size()) {
+            System.out.print(list.get(i));
+            i++;
+        }
+        System.out.println();
+    }
+*/
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the TEXT");
